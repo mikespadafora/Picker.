25006b3473cb09019ac0aa279beeddb21084cf82
mikespadafora <michaelspadafora@outlook.com>
2023-04-21 22:08:57 -0400

    Started configuring nav header. Might use custom component

--- src/routes/MainStack.tsx
+++ src/routes/MainStack.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Image } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackNavigationProp } from "@react-navigation/stack";
 import SearchRadius from "../ui/pages/SearchRadius";
@@ -11,13 +12,28 @@ export type MainStackParamList = {
   Keywords: undefined;
 };
 
+const imagePath = require("../../assets/img/logo.png");
+
+const globalScreenOptions = {
+  headerTitle: () => <Image source={imagePath} style={{ width: 40, height: 40 }} />,
+  headerStyle: {
+    backgroundColor: "white",
+    elevation: 0,
+    shadowOpacity: 0,
+    shadowColor: "transparent",
+    height: 100,
+  },
+  headerShadowVisible: false,
+  headerBackTitleVisible: false,
+};
+
 export type MainStackNavigation = StackNavigationProp<MainStackParamList>;
 
 const Stack = createNativeStackNavigator<MainStackParamList>();
 
 const MainStack = ({ locationDenied }: { locationDenied: boolean }) => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={globalScreenOptions}>
       <Stack.Screen name="SearchRadius" component={SearchRadius} initialParams={{ locationDenied: locationDenied }} />
       <Stack.Screen name="Keywords" component={Keywords} />
       {/* <Stack.Screen name="RestaurantSelection" component={RestaurantSelection} />
